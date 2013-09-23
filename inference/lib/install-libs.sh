4b7fc51438efda24fa3f7c0b9dbef355dba548ca
niranjan <niranjan@cs.washington.edu>
2013-09-23 11:33:39 -0700

    Revert "Fix for xerces jar versioning issue and a missing woodstox library"
    This reverts commit b7e663b76bf4d9e68aaaabafa31b1e2100abcc81.
    
    Will try to push jars to knowitall maven repo instead.

--- inference/lib/install-libs.sh
+++
@@ -1,3 +0,0 @@
-mvn install:install-file -Dfile=wstx-asl-3.2.9.jar -DgroupId=org.codehaus.woodstox -DartifactId=wstx-asl -Dversion=3.2.9 -Dpackaging=jar
-mvn install:install-file -Dfile=xercesImpl.jar -DgroupId=xerces -DartifactId=xercesImpl -Dversion=2.11.0 -Dpackaging=jar
-mvn install:install-file -Dfile=xml-apis.jar -DgroupId=xml-apis -DartifactId=xml-apis -Dversion=1.4.01 -Dpackaging=jar
