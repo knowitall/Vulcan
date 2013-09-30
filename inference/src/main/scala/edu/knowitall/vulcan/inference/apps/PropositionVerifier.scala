dad1a69a09f4569ddbcc861d9ea813a64a5c1d6a
niranjan <niranjan@cs.washington.edu>
2013-09-30 14:15:51 -0700

    adding cnc host/port information to verifier

--- inference/src/main/scala/edu/knowitall/vulcan/inference/apps/PropositionVerifier.scala
+++ inference/src/main/scala/edu/knowitall/vulcan/inference/apps/PropositionVerifier.scala
@@ -9,47 +9,54 @@ package edu.knowitall.vulcan.inference.apps
  */
 
 
-import edu.knowitall.vulcan.inference.evidence.{PatternEvidenceFinder, Evidence, EvidenceFinder}
+import edu.knowitall.vulcan.inference.evidence.{TextualAxiomsFinder, AxiomsFinder, Evidence}
 import edu.knowitall.vulcan.inference.proposition.Proposition
-import edu.knowitall.vulcan.inference.kb.{LogicRules, WeightedRule, Predicate}
-import edu.knowitall.vulcan.inference.openie.SolrSearchWrapper
-import edu.knowitall.vulcan.inference.utils.TupleHelper
+import edu.knowitall.vulcan.inference.kb._
 import edu.knowitall.vulcan.inference.mln.tuffyimpl._
-import java.io.File
 import org.slf4j.LoggerFactory
+import edu.knowitall.vulcan.inference.mln.tuffyimpl.InferenceResults
+import scala.Some
+import java.io.File
 import edu.knowitall.vulcan.inference.mln.tuffyimpl.InferenceResults
 import edu.knowitall.vulcan.inference.evidence.Evidence
 import scala.Some
+import scopt.mutable.OptionParser
+import edu.knowitall.vulcan.inference.utils.TupleHelper
 
 
-class PropositionVerifier(finder:EvidenceFinder,
+class PropositionVerifier(finders:Seq[AxiomsFinder],
                           tuffy:TuffyWrapper,
                           rules:Seq[WeightedRule],
-                          tempDirectory:String){
+                          tempDirectory:String,
+                          host:String,
+                          port:Int){
 
 
   val logger = LoggerFactory.getLogger(this.getClass)
-  //val conf = new TuffyArgs(tempDirectory, tuffy)
 
+  def findEvidence(proposition:Proposition)  =  {
+    val axioms = finders.flatMap(_.find(proposition))
+    new Evidence(proposition, axioms, rules)
+  }
+  def exportEvidence(evidence:Evidence) = {
+    val instance = MLNInstanceIO.fromEvidence(evidence)
+    TuffyFormatter.exportToDirectory(instance, tempDirectory)
+
+  }
   def verify(proposition:Proposition) = {
-    finder.find(proposition) match {
-      case Some(evidence:Evidence) => {
-        val newEvidence = new Evidence(evidence.proposition, evidence.axioms, evidence.rules ++ rules)
-        val instance = MLNInstanceIO.fromEvidence(newEvidence)
-        TuffyFormatter.exportToDirectory(instance, tempDirectory)
-        tuffy.runTuffyNative(tempDirectory) match {
-          case Some(results:InferenceResults) => {
-            "Output: " + results.marginals.mkString("\n")
-          }
-          case None => {
-            logger.error("No results from inference.")
-            "No results from inference."
-          }
-        }
+    val evidence = findEvidence(proposition)
+    exportEvidence(evidence)
+    runTuffy()
+  }
+
+  def runTuffy(): String = {
+    tuffy.runTuffyNative(tempDirectory) match {
+      case Some(results: InferenceResults) => {
+        "Output: " + results.marginals.mkString("\n")
       }
       case None => {
-        println("No evidence found for: " + proposition.text)
-        "No evidence found for: " + proposition.text
+        logger.error("No results from inference.")
+        "No results from inference."
       }
     }
   }
@@ -58,24 +65,54 @@ class PropositionVerifier(finder:EvidenceFinder,
 object PropositionVerifier {
 
 
+  val logger = LoggerFactory.getLogger(this.getClass)
 
   def main(args:Array[String]){
 
-    val finder = new PatternEvidenceFinder(SolrSearchWrapper.getInstance(args(0)))
-    val tuffy = new TuffyWrapper(args(1))
-    val verifier = new PropositionVerifier(finder, tuffy, LogicRules.fromFile(new File(args(2))), args(3))
+    var endpoint = ""
+    var tuffyConfFile = ""
+    var rulesFile = ""
+    var tempDir = ""
+    var host = ""
+    var port = 0
+    val parser = new OptionParser() {
+      arg("endpoint", "Textual evidence client endpoint.", {str => endpoint = str})
+      arg("tuffyConfFile", "Tuffy conf file.", {str => tuffyConfFile = str})
+      arg("rulesFile", "Rules file.", {str => rulesFile = str})
+      arg("tempDir", "Temporary directory to store MLN instance files.", {str => tempDir = str})
+      arg("host", "CNC Host.", {str => host = str})
+      arg("port", "Port", {str => port = str.toInt})
+    }
+
+    if (!parser.parse(args)) return
+
+    val verifier = verifierInstance(endpoint, tuffyConfFile, rulesFile, tempDir, host, port)
+    import TupleHelper._
     val tupleRegex = """\((.*?), (.*?), (.*)\)""".r
     println("Enter Prop[(arg1, rel, arg2)]: ")
-    import TupleHelper._
-    for( ln <- io.Source.stdin.getLines ) {
+    for( ln <- io.Source.stdin.getLines() ) {
       val tupleRegex(arg1, rel, arg2) = ln
       val pred = new Predicate(from(arg1, rel, arg2), 1.0)
-      verifier.verify(new Proposition(Seq[Predicate](), pred))
+      val prop = new Proposition(Seq[Predicate](), pred)
+      val evidence = verifier.findEvidence(prop)
+      verifier.exportEvidence(evidence)
+      //verifier.runTuffy()
       println
       println
       println("Enter Prop[(arg1, rel, arg2)]: ")
     }
   }
 
+
+  def verifierInstance(endpoint: String, tuffyConfFile: String, rulesFile: String, tempDir: String, host:String, port:Int) = {
+    val taf = new TextualAxiomsFinder(endpoint)
+    val kbf = new KBAxiomFinder(new WordnetKB :: new CNCategorizerKB(host, port) :: Nil)
+    val finders = Seq(taf, kbf)
+    val tuffy = new TuffyWrapper(tuffyConfFile)
+    val file = new File(rulesFile)
+    val rules = LogicRules.fromFile(file)
+    logger.info("# of rules loaded = %d".format(rules.size))
+    new PropositionVerifier(finders, tuffy, rules, tempDir, host, port)
+  }
 }
 
