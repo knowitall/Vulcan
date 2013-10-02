880e861a360b8297d0274478649711fdd91e84f2
niranjan <niranjan@cs.washington.edu>
2013-10-02 09:12:12 -0700

    wildcard queries for tuple added

--- inference/src/main/scala/edu/knowitall/vulcan/inference/evidence/TextualAxiomsFinder.scala
+++ inference/src/main/scala/edu/knowitall/vulcan/inference/evidence/TextualAxiomsFinder.scala
@@ -10,12 +10,13 @@ package edu.knowitall.vulcan.inference.evidence
 
 
 import scala.collection.JavaConversions._
-import edu.knowitall.vulcan.common.Tuple
+import edu.knowitall.vulcan.common.{Arg, Tuple}
 import edu.knowitall.vulcan.evidence.TextualEvidenceClient
 import edu.knowitall.vulcan.evidence.query.{TupleQuery, QueryBuilder}
 import edu.knowitall.vulcan.inference.proposition.Proposition
 import edu.knowitall.vulcan.inference.kb.{WeightedRule, Axiom, Predicate}
 import org.slf4j.LoggerFactory
+import edu.knowitall.vulcan.inference.utils.TupleHelper
 
 object TextualAxiomsFinder{
 
@@ -37,8 +38,19 @@ class TextualAxiomsFinder(endpoint:String) extends AxiomsFinder{
 
   val corpusQuery = TupleQuery.corpusQuery(Seq("glossary", "studyguide", "clueweb"))
 
+  def fieldQueries(tuple:Tuple) = {
+    val arg1 = tuple.arg1.text
+    val rel = tuple.rel.text
+    val arg2 = tuple.arg2s.map(_.text).mkString(" ")
+    tuple::
+    TupleHelper.from(arg1, rel, "")::
+    TupleHelper.from(arg1, "", arg2)::
+    TupleHelper.from("", rel, arg2)::Nil
+
+  }
 
   def find(proposition:Proposition) = {
+    //val queries = fieldQueries(proposition.consequent.tuple).map(QueryBuilder.and(TupleQuery.partialMatchQuery()))
     val query = QueryBuilder.and(TupleQuery.matchAnyQuery(proposition.consequent.tuple), corpusQuery)
     logger.info("Query: " + query)
     val resultsPage = client.query(query, start=0, rows=100)
