360133459e5151bbdf7590c0bb7c99c085de3fcb
niranjan <niranjan@cs.washington.edu>
2013-10-14 13:17:29 -0700

    changes to use remote srl and parse server

--- inference/src/main/scala/edu/knowitall/vulcan/inference/openie/OpenIEWrapper.scala
+++ inference/src/main/scala/edu/knowitall/vulcan/inference/openie/OpenIEWrapper.scala
@@ -16,7 +16,9 @@ object OpenIEWrapper{
 
   def main(args:Array[String]){
     println("Sentence: ")
-    val extractor = instance(args(0))
+    val parser = if(args.size > 1) Some(args(1)) else None
+    val srl = if(args.size > 2) Some(args(2)) else None
+    val extractor = instance(args(0), parser, srl)
     var count = 0
     for( ln <- io.Source.stdin.getLines ) {
       val instances = extractor.extract(ln, "stdin", () => count.toString)
